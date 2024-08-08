9dbca0523255524d65a55871f2f049be29fa6760
GustavoHenriqueDEV <gubochniavelho@gmail.com>
2024-08-08 00:45:12 -0300

    Estilo-input

---
+++ inputs.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <header>
+
+        <h1>Formulario</h1>
+        <section>
+            <form action="cad.php" method="get">
+                <label for="nome">Nome</label>
+                <input type="text" name="nome" id="idnome">
+                <label for="sobrenome">sobrenome</label>
+                <input type="text" name="sobrenome" id="sobrenome">
+                <input type="submit" value="Enviar">
+            </form>
+        </section>
+
+    </header>
+</body>
+
+</html>
